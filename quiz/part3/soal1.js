4394ac46e0eec1af567225d7b9317d2e6fc23003
tikodjati <tikodjati@gmail.com>
2026-01-06 19:20:01 +0700

    Buat file .js untuk tantangan part3

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,22 @@
+function changeMe(arr) {
+  // you can only write your code here!
+    let data = {};
+    for(let i = 0; i < arr.length; i++){
+
+    }
+}
+
+// TEST CASES
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+// Christ Evans: { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 41 } 2023 - 1982 = 41 kan yak wkwk
+// Robert Downey: { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+
+//intinya bagaimana cara kalian bisa menampilkan output diatas, sebebas dan sekreatif kalian.
+
+changeMe([]); // ""
